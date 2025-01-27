ddfbd4f11edceff2603236a0ffefdbab20ebaf78
ianco <ianco.so@gmail.com>
2025-01-27 03:24:31 -0300

    :sparkles: feat(foto): testando adição de fotos

---
+++ lib/models/contact.dart
@@ -0,0 +1,27 @@
+// // A contact model class
+
+// import 'package:flutter/material.dart';
+
+// import 'status.dart';
+
+// class Contact {
+//   String firstName;
+//   String lastName;
+//   String email;
+//   String phone;
+//   Status status;
+//   Location location;
+//   String picture; // A Base64 encoded image
+  
+
+
+//   Contact({this.name, this.email, this.phone});
+
+//   factory Contact.fromJson(Map<String, dynamic> json) {
+//     return Contact(
+//       name: json['name'],
+//       email: json['email'],
+//       phone: json['phone'],
+//     );
+//   }
+// }
---
+++ lib/models/status.dart
@@ -0,0 +1,4 @@
+// Contact Status could be: BLOCKED, FAVORITE OR NORMAL
+
+// ignore: constant_identifier_names
+enum Status { BLOCKED, FAVORITE, NORMAL }
